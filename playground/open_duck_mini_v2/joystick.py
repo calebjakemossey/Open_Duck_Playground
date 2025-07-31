5989348c1fd2d296510b8a49b9a479439bae3864
apirrone <awanegenebayo@gmail.com>
2025-07-31 22:28:23 +0200

    no randomization yet

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -707,7 +707,7 @@ class Joystick(open_duck_mini_v2_base.OpenDuckMiniV2Env):
 
         # With 10% chance, set everything to zero.
         return jp.where(
-            jax.random.bernoulli(rng4, p=0.1),
+            jax.random.bernoulli(rng4, p=0.0),
             jp.zeros(7),
             jp.hstack(
                 [
