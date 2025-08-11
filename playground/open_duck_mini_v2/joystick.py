54f00d32cab89e84af813dac580bba4603af66e1
apirrone <awanegenebayo@gmail.com>
2025-08-11 10:38:10 +0200

    trying without mask head

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -44,7 +44,7 @@ from playground.open_duck_mini_v2.custom_rewards import reward_imitation
 # if set to false, won't require the reference data to be present and won't compute the reference motions polynoms for nothing
 USE_IMITATION_REWARD = True
 USE_MOTOR_SPEED_LIMITS = True
-MASK_HEAD = True
+MASK_HEAD = False
 
 
 def default_config() -> config_dict.ConfigDict:
